5ecd57159a31ab08481b141911f2fb4854b0d1d5
Talha _Office <abutalha.officialac@gmail.com>
2022-06-08 14:57:09 +0600

    first commit

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+import assets from "./assets";
+import { KcsData } from "./dummy";
+import { COLORS, FONTS, SHADOWS, SIZES } from "./theme";
+
+export { assets, COLORS, SHADOWS, SIZES, FONTS, KcsData };
